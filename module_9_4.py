a350b594cc88b7c6076831c56ed0a14f11c671dc
OlgaV0539 <olv-personal@mail.ru>
2024-10-30 16:14:21 +0700

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,38 @@
+import random
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(f"{data}\n")
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+class MysticBall:
+    def __init__(self, words):
+        if not isinstance(words, list) or not all(isinstance(word, str)
+                                                  for word in words):
+            raise ValueError("words должен быть списком строк")
+        self.words = words
+
+
+    def call(self):
+        return random.choice(self.words)
+
+
+first_ball = MysticBall(['Да', 'Нет', 'Наверное'])
+print(first_ball.call())
+print(first_ball.call())
+print(first_ball.call())
